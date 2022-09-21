2880c1f4d4b77e863bf2524a89b1463fb61e3a84
Tony-Castro <114043633+Tony-Castro@users.noreply.github.com>
2022-09-20 23:26:14 -0700

    Add files via upload

---
+++ Donuts.py
@@ -0,0 +1,10 @@
+import sys
+
+a = int(input('Enter Number of Donuts \n'))
+
+if a >= 10:
+    print("Number of Donuts Many")
+elif a < 10:
+    print("Number of Donuts is " + (str(a)))
+
+input('Press Enter to close')
